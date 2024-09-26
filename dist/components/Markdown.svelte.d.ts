6b24e7e51e7314c7b1af24a6d430d2a941424b1e
Matthew Gibbons <mhgibbons@me.com>
2024-09-26 06:53:10 +0000

    Release 0.0.1

---
+++ dist/components/Markdown.svelte.d.ts
@@ -0,0 +1,4 @@
+import { Markdown } from '@accuser/svelte-markdown-provider';
+declare const Markdown: any;
+type Markdown = InstanceType<typeof Markdown>;
+export default Markdown;
---
+++ dist/index.js
@@ -0,0 +1,2 @@
+export { Node, getMarkdownContext } from '@accuser/svelte-markdown-provider';
+export { default as Markdown } from './components/Markdown.svelte';
