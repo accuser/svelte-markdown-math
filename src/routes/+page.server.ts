87be206fad32a8a2378fc404854a6ddccdba4401
Matthew Gibbons <mhgibbons@me.com>
2024-09-27 05:34:58 +0000

    Correct source

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,7 +4,7 @@ import type { PageServerLoad } from './$types.js';
 export const csr = false;
 
 export const load = (async () => {
-	const src = fs.readFileSync('content/gfm.md').toString('utf-8');
+	const src = fs.readFileSync('content/math.md').toString('utf-8');
 
 	return { src };
 }) satisfies PageServerLoad;
