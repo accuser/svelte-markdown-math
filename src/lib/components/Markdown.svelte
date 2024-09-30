50db93a0a1240df1603bc50f01d27da8b60da47a
Matthew Gibbons <mhgibbons@me.com>
2024-09-30 08:53:28 +0000

    Only destructure props for modification

--- src/lib/components/Markdown.svelte
+++ src/lib/components/Markdown.svelte
@@ -6,9 +6,9 @@
 	import InlineMath from './InlineMath.svelte';
 	import Math from './Math.svelte';
 
-	type Props = ComponentProps<Markdown>;
+	type Props = ComponentProps<typeof Markdown>;
 
-	let { ast, components: _components, directives, options: _options, src }: Props = $props();
+	let { components: _components, options: _options, ...props }: Props = $props();
 
 	let components = $derived.by(() => {
 		const { ...rest } = _components ?? {};
@@ -21,14 +21,14 @@
 	});
 
 	let options = $derived.by(() => {
-		const { extensions = [], mdastExtensions = [], ...rest } = _options ?? {};
+		const { extensions, mdastExtensions, ...rest } = _options ?? {};
 
 		return {
-			extensions: [...extensions, math()],
-			mdastExtensions: [...mdastExtensions, mathFromMarkdown()],
+			extensions: [...(extensions ?? []), math()],
+			mdastExtensions: [...(mdastExtensions ?? []), mathFromMarkdown()],
 			...rest
 		};
 	});
 </script>
 
-<Markdown {ast} {components} {directives} {options} {src} />
+<Markdown {...props} {components} {options} />
