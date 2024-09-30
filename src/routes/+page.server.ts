d44564f349e8a5be7692ea5bd3e27d87f50ea901
Matthew Gibbons <mhgibbons@me.com>
2024-09-30 08:53:03 +0000

    Don't disable client-side rendering

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,8 @@
 import fs from 'node:fs';
 import type { PageServerLoad } from './$types.js';
 
-export const csr = false;
-
 export const load = (async () => {
-	const src = fs.readFileSync('content/math.md').toString('utf-8');
+	const src = fs.readFileSync('content/README.md').toString('utf-8');
 
 	return { src };
 }) satisfies PageServerLoad;
